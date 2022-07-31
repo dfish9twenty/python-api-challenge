307e558d3e15ef50ea1ccbf10962320a9ece9afb
Andrew Fisher <105055707+dfish9twenty@users.noreply.github.com>
2022-07-31 09:27:39 -0500

    push code and images

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ecd7676b8f0da0d04c7924800ee53e0f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCPpaq589AxLJLNzgnBc_x1eTBmlhGAvRg"
